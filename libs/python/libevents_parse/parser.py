00eab49581f4cdd3f35105b0f0a166c4c89b09ff
Beat Küng <beat-kueng@gmx.net>
2020-11-19 10:48:04 +0100

    python: add parsing library + tests

---
+++ libs/python/libevents_parse/parser.py
@@ -0,0 +1,300 @@
+""" python events parsing libary """
+import json
+from typing import Tuple
+import struct
+import re
+
+_base_types = {
+    "uint8_t": {"size": 1, "unpack": struct.Struct('<B').unpack},
+    "int8_t": {"size": 1, "unpack": struct.Struct('<b').unpack},
+    "uint16_t": {"size": 2, "unpack": struct.Struct('<H').unpack},
+    "int16_t": {"size": 2, "unpack": struct.Struct('<h').unpack},
+    "uint32_t": {"size": 4, "unpack": struct.Struct('<I').unpack},
+    "int32_t": {"size": 4, "unpack": struct.Struct('<i').unpack},
+    "uint64_t": {"size": 8, "unpack": struct.Struct('<Q').unpack},
+    "int64_t": {"size": 8, "unpack": struct.Struct('<q').unpack},
+    "float": {"size": 4, "unpack": struct.Struct('<f').unpack},
+    }
+
+class ParserException(Exception):
+    """
+    Exception to indicate a parser error.
+    """
+
+class ParsedEvent:
+    """ representation of a parsed event (with metadata) """
+
+    def __init__(self, event: dict, event_extras: dict, arguments: bytes,
+                 events: dict, profile: str, debug: bool):
+        self._event = event
+        self._event_extras = event_extras
+        self._arguments = arguments
+        self._events = events
+        self._profile = profile
+        self._debug = debug
+
+        self._values = [] # tuples of (value, enum)
+        offset = 0
+        for arg in self._event.get('arguments', []):
+            base_type, enum = self._get_basetype(arg)
+            size = _base_types[base_type]["size"]
+            value, = _base_types[base_type]["unpack"](self._arguments[offset:offset+size])
+            self._values.append((value, enum))
+            offset += size
+
+        self._debug_print("Event: {}, {}, values={}".format(self.event_id(),
+                                                            self.name(), self._values))
+
+    def _get_basetype(self, event_arg: dict) -> Tuple[str, dict]:
+        """ get the basic type, and the enum definition if it's an enum """
+        arg_type = event_arg['type']
+        if arg_type in _base_types:
+            return (arg_type, None)
+        # enum
+        if '::' in arg_type:
+            enum_type = arg_type
+        else:
+            enum_type = self.namespace() + '::' + arg_type
+        enum = self._event_extras['enums'][enum_type]
+        return (enum['type'], enum)
+
+
+    def _debug_print(self, msg: str):
+        """ debug output """
+        if self._debug:
+            print(msg)
+
+    def event_id(self) -> int:
+        """ get event ID """
+        return self._event_extras['id']
+
+    def name(self) -> str:
+        """ get event name """
+        return self._event['name']
+
+    def namespace(self) -> str:
+        """ get event namespace """
+        return self._event_extras['namespace']
+
+    def message(self) -> str:
+        """ get event message """
+        return self._process_message(self._event['message'])
+
+    def description(self) -> str:
+        """ get event description """
+        if 'description' not in self._event:
+            return ''
+        return self._process_message(self._event['description'])
+
+    def group(self) -> str:
+        """ get event group name """
+        return self._event_extras['group']
+
+    def num_arguments(self) -> int:
+        """ get number of arguments """
+        if 'arguments' in self._event:
+            return len(self._event['arguments'])
+        return 0
+
+    def argument_value(self, idx: int):
+        """ get argument value (not enum type) """
+        return self._values[idx][0]
+
+    def argument_display_value(self, idx: int):
+        """ get argument to display (for enum's it's the enum entry/bits) """
+        value, enum = self._values[idx]
+        if enum is None:
+            return value
+
+        if enum.get('is_bitfield', False):
+            size = _base_types[enum['type']]['size']
+            ret_value = ""
+            had_bit = False
+            for i in range(size*8):
+                bit = 1<<i
+                if value & bit:
+                    if had_bit:
+                        ret_value += "|"
+                    found = False
+                    for entry in enum['entries']:
+                        if entry['value'] == bit:
+                            ret_value += entry['description']
+                            found = True
+                            break
+                    if not found:
+                        ret_value += '(unknown: {})'.format(value)
+                    had_bit = True
+            return ret_value
+
+        for entry in enum['entries']:
+            # could turn this into a map to make this more efficient...
+            if entry['value'] == value:
+                return entry['description']
+        return '(unknown: {})'.format(value)
+
+    def _process_message(self, msg: str) -> str:
+        """
+         * Supported parsing:
+         * - characters can be escaped with \\, e.g. '\\<', '\\{'
+         * - tags:
+         *   - <profile name="[!]NAME">CONTENT</profile>
+         *   - <a [href="URL"]>CONTENT</a>
+         *     if href is not found, use CONTENT as url
+         *   - <param>PARAM_NAME</param>
+         *   - unknown tags are ignored (including content)
+         *   - no nested tags of the same type
+         * - arguments: following python syntax, with 1-based indexing (instead of 0)
+         *   - general form: {ARG_IDX[:.NUM_DECIMAL_DIGITS]}
+         * - returned string will be trimmed (removed whitespace)
+         """
+        i = 0
+        while i < len(msg):
+
+            if msg[i] == '\\': # escaped character
+                msg = msg[:i]+msg[i+1:]
+                i += 1
+                continue
+
+            if msg[i] == '<':
+                # extract tag with 1 optional argument
+                # (assumes the argument does not contain an escaped ")
+                m = re.match(r"^<([a-z]+)(?: ([a-z]+)=\"([^\"]*)\")?>(.)", msg[i:], re.DOTALL)
+                if not m:
+                    self._debug_print("Error: invalid or unsupported format")
+                    i += 1
+                    continue
+
+                tag_name = m.group(1)
+                content_start_idx = m.start(4)
+                end_tag_idx = msg.find('</'+tag_name+'>', i+content_start_idx)
+                if end_tag_idx == -1:
+                    self._debug_print("Error: invalid format")
+                    i += 1
+                    continue
+
+                tag_content = msg[i+content_start_idx:end_tag_idx]
+                num_skip = 0
+                if tag_name == "profile":
+                    profile = ''
+                    if m.group(3) is not None:
+                        profile = m.group(3)
+                    if profile.startswith('!'):
+                        remove_content = self._profile == profile[1:]
+                    else:
+                        remove_content = self._profile != profile
+                    if remove_content:
+                        tag_content = ''
+                elif tag_name == 'a':
+                    if m.group(3) is None:
+                        url = tag_content
+                    else:
+                        url = m.group(3)
+                    tag_content = self._event_extras['url-formatter'](tag_content, url)
+                    num_skip = len(tag_content)
+                elif tag_name == 'param':
+                    tag_content = self._event_extras['param-formatter'](tag_content)
+                    num_skip = len(tag_content)
+                else: # unknown tag
+                    self._debug_print("unknown tag: {}".format(tag_name))
+                    tag_content = ''
+
+                msg = msg[:i] + tag_content + msg[end_tag_idx+len(tag_name)+3:]
+                i += num_skip
+
+            elif msg[i] == '{':
+                arg_end_idx = msg.find('}', i)
+                if arg_end_idx == -1:
+                    i += 1
+                    self._debug_print("Error: } not found")
+                    continue
+                arg = msg[i+1:arg_end_idx]
+                m = re.match(r"^(\d+)(?::(?:\.(\d+))?)?$", arg)
+                if not m:
+                    i += 1
+                    self._debug_print("Error: unknown argument format")
+                    continue
+                arg_idx = int(m.group(1)) - 1
+                if arg_idx < 0 or arg_idx >= len(self._values):
+                    i += 1
+                    self._debug_print("Error: invalid argument index")
+                    continue
+                print_fmt = "{}"
+                if m.group(2) is not None:
+                    print_fmt = "{:."+m.group(2)+"f}"
+                arg_value = print_fmt.format(self.argument_display_value(arg_idx))
+                msg = msg[:i] + arg_value + msg[arg_end_idx+1:]
+                i += len(arg_value)
+            else:
+                i += 1
+        return msg.strip()
+
+
+class Parser:
+    """ main parsing class """
+
+    def __init__(self, debug: bool = False):
+        self._profile = "dev"
+        self._events = {}
+        self._enums = {}
+        self._debug = debug
+        self._url_formatter = lambda content, url: url
+        self._param_formatter = lambda param_name: param_name
+
+    def load_definition_file(self, json_file: str):
+        """ load event definitions from a json file """
+        with open(json_file, 'r') as j:
+            self.load_definitions(json.load(j))
+
+    def load_definitions(self, json_data: dict):
+        """ load event definitions from a dict """
+        # TODO: merge with self._events in case we already have definitions
+
+        version = json_data.get('version', 0)
+        if version != 1:
+            raise ParserException("Unexpected version {}".format(version))
+        self._events = json_data
+        # store enums separately for faster access
+        for comp in self._events.get("components", []):
+            namespace = comp["namespace"]
+            for enum in comp.get("enums", []):
+                self._enums[namespace+'::'+enum['name']] = enum
+
+    def parse(self, event_id: int, arguments: bytes) -> ParsedEvent:
+        """ Parse event from event ID and arguments """
+        # Find ID
+        events = self._events
+        for comp in events.get("components", []):
+            comp_id = comp["component_id"]
+            for group in comp.get("event_groups", []):
+                for event in group.get("events", []):
+                    cur_id = (comp_id << 24) | event['sub_id']
+                    if event_id == cur_id:
+                        # pass some additional info
+                        event_extras = {
+                            'id': cur_id,
+                            'namespace': comp["namespace"],
+                            'group': group["name"],
+                            'enums': self._enums,
+                            'url-formatter': self._url_formatter,
+                            'param-formatter': self._param_formatter,
+                            }
+                        return ParsedEvent(event, event_extras, arguments, events,
+                                           self._profile, self._debug)
+        return None
+
+    def set_profile(self, profile: str):
+        """ set the profile name """
+        self._profile = profile
+
+    def set_url_formatter(self, formatter):
+        """ set url formatter
+            :param formatter: lambda with (content: str, url: str) args
+        """
+        self._url_formatter = formatter
+
+    def set_param_formatter(self, formatter):
+        """ set param formatter
+            :param formatter: lambda with (content: str) args
+        """
+        self._param_formatter = formatter
+
